39b69e02c1c6be4b07eb390f3636e25a57d4955e
rajofearth <rajofearth@proton.me>
2025-11-08 11:44:53 +0530

    Add HomeMenubar component to layout and wrap children in main tag; update Home component structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { HomeMenubar } from "@/components/home/menubar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <main>
+          <HomeMenubar />
+          {children}</main>
       </body>
     </html>
   );
