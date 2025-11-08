85d3099283a8f3e740e09513373e6a2876c5a41b
rajofearth <rajofearth@proton.me>
2025-11-08 12:34:47 +0530

    Add AppSidebar component and integrate it into Home component; implement Sidebar functionality with new UI components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,34 @@
+import { AppSidebar } from "@/components/home/app-sidebar"
+import { Separator } from "@/components/ui/separator"
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar"
+import { HomeMenubar } from "@/components/home/menubar"
+
 export default function Home() {
   return (
-    <>
-    <h1>Hello World</h1>
-    </>
-  );
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="bg-background sticky top-0 flex shrink-0 items-center gap-2 border-b p-4">
+          <SidebarTrigger className="-ml-1" />
+          <Separator
+            orientation="vertical"
+            className="mr-2 data-[orientation=vertical]:h-4"
+          />
+          <HomeMenubar />
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4">
+          {Array.from({ length: 24 }).map((_, index) => (
+            <div
+              key={index}
+              className="bg-muted/50 aspect-video h-12 w-full rounded-lg"
+            />
+          ))}
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  )
 }
